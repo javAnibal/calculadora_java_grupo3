81ea3d8971fecfa2a5c14db197c21df320672c31
anibaL <a88anibal@gmail.com>
2025-09-29 12:18:20 +0200

    Function division completa

--- Calculadora/src/main/FunctionDivision.java
+++ Calculadora/src/main/FunctionDivision.java
@@ -2,5 +2,19 @@ package main;
 
 public class FunctionDivision {
 
-    
+
+    public static int dividir(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir entre cero");
+        }
+        return a / b;
+    }
+
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir entre cero");
+        }
+        return a / b;
+    }
 }
